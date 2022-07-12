b33e837f12e6967cc9c72389a4e54e162a79d142
Cristian Beltran <cbeltranc6@gmail.com>
2022-07-12 13:27:55 -0400

    Sockets Implementado

--- classes/server.ts
+++ classes/server.ts
@@ -1,17 +1,44 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/enviorement';
+import socketIO from 'socket.io';
+import {createServer}  from 'http';
+
+import * as socket from '../sockets/socket';
 
 export default class Server{
+
+    private static _instance: Server;
 
     public app: express.Application;
     public port: number;
 
-    constructor(){
+    public io: socketIO.Server;
+    private httpServer;
+    private constructor(){
         this.app = express();
         this.port = SERVER_PORT;
+        this.httpServer = createServer(this.app);
+
+        this.io = new socketIO.Server( this.httpServer, { cors: { origin: true, credentials: true } } );
+
+        this.escucharSockets();
+    }
+
+    public static getInstance(){
+        return this._instance || (this._instance = new this());
+    }
+
+    private escucharSockets(){
+        console.log('Escuchando conexiones - sockets');
+        this.io.on('connection', cliente =>{
+            console.log('Cliente conectado');
+
+            socket.desconectar(cliente);
+            socket.mensaje(cliente, this.io);
+        })
     }
 
     start( callback: Function ){
-        this.app.listen(this.port, callback);
+        this.httpServer.listen(this.port, callback());
     }
 }
